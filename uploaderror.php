0cdd6f3022f59db6dfff2db346585fd448322527
petrozz1 <kjpetrozzi@gmail.com>
2016-04-04 13:47:43 -0400

    Added upload error pages
    simple using the php? command.

---
+++ uploaderror.php
@@ -0,0 +1,35 @@
+<?php include_once 'header.php'; ?>
+
+<!-- Page Content -->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12">
+
+
+            <!-- Page Heading -->
+            <div class="text-center">
+                <h1>Something happened!</h1>
+            </div>
+            <hr>
+
+            <p class="lead text-center">
+				<?php $error = trim($_GET['error']);
+					if ($error == 2){
+						echo "Sorry, that file is too large! Your file was not uploaded.";
+					}
+					else if ($error == 3){
+						echo "Sorry, only PDFs can be uploaded! Your file was not uploaded.";	
+					}
+					else{
+						echo "Sorry, something went wrong and your file was not uploaded.";
+					}
+				?>
+			</p>
+
+        </div>
+    </div>
+    <!-- /.row -->
+</div>
+<!-- /.container-fluid -->
+
+<?php include_once 'footer.php' ?>
