098f99e9e901fb87824e56d4f3ff125706815c11
David Mulatti <david@mulatti.ca>
2016-04-04 17:43:01 -0400

    Fix resume upload layout and css

--- contact.php
+++ contact.php
@@ -68,7 +68,7 @@ if ($_SESSION['logged_in'] > 0){
             </p>
             <br>
 
-					<div class="well">
+					<div class="well form-center">
 						<form id="contactForm" name="contactForm" class="required" method="POST" action="contact_go.php">
 							<div class="form-group">
 								<label for="contactName">Name</label>
--- resumeupload.php
+++ resumeupload.php
@@ -5,51 +5,54 @@ session_start();
 
 	<div class="container-fluid">
         <div class="row">
-		
-            <div class="col-sm-12">
-
 				<!-- Page Heading -->
                 <div class="text-center">
                     <h1>Resume Upload</h1>
                 </div>
+
+
+
+			<div class="col-sm-8">
+				<object align = "left" height="750px" width="100%" data="resume/<?php echo $_SESSION['uwinid'] ?>.pdf"></object>
 			</div>
-			
-			<object class = "pdf" align = "left" width="725" height="775" data="resume/<?php echo $_SESSION['uwinid'] ?>.pdf"></object>
-			
-			<div class="col-sm-12">
-				<div class="well-pdf">			
+
+
+
+			<div class="col-sm-4">
+				<div class="well">
+
 					<form id='upload' action='upload.php' method='post' enctype="multipart/form-data">
-									
+
 						<div class="form-group" >
 							<label for='uploaddescription' >Description:</label>
-							
+
 							<!-- php for description -->
-							<?php $stmt = $db->prepare("SELECT description FROM users WHERE uwinid=?");
-								$stmt->bind_param("s", $_SESSION['uwinid']);
-								$result = $stmt->execute();
-								$stmt->store_result();
-								$count = $stmt->num_rows;
-								$stmt->bind_result($descript);
-								$stmt->fetch();									
-							?>
-							<textarea align="right" class="form-control-large" name='uploaddescription' 
-							id='uploaddescription' cols= "50" rows = "10" form="upload"><?php echo $descript;?></textarea> 
+							<?php $stmt = $db->prepare('SELECT description FROM users WHERE uwinid=?');
+                                $stmt->bind_param('s', $_SESSION['uwinid']);
+                                $result = $stmt->execute();
+                                $stmt->store_result();
+                                $count = $stmt->num_rows;
+                                $stmt->bind_result($descript);
+                                $stmt->fetch();
+                            ?>
+							<textarea align="right" class="form-control-large" name='uploaddescription'
+							id='uploaddescription' cols= "50" rows = "10" form="upload"><?php echo $descript;?></textarea>
 						</div>
-			
+
 						<div class="form-group">
 							<label for='fileToUpload' >File:</label>
 							<input class="form-control" type="file" name="fileToUpload" id="fileToUpload">
 						</div>
-						
-					
+
+
 						<input class="btn btn-primary" type='submit' name='uploadButton' value='Upload' />
-						
+
 					</form>
 				</div>
-		
+
 	        </div>
         </div> <!-- /.row -->
-		
+
 	</div> <!-- /.container-fluid -->
 
 <?php include_once 'footer.php'; ?>
